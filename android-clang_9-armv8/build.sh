4d0e05b53638a29f8d1ae4b6788b5f0626dae347
zhijia <shelpermisc@gmail.com>
2020-06-14 19:40:16 -0400

    add android clang 9 for armv8

---
+++ android-clang_9-armv8/build.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+sudo docker build --no-cache -t shelper/android-clang9-armv8 .
---
+++ android-clang_9-armv8/run.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+sudo docker run --rm -v ~/.conan/data:/home/conan/.conan/data -it shelper/android-clang9-armv8 /bin/bash
